c3ec9b8134555497dac93b75e0444aae11c40874
Oleksandr Dyakov <dyakovkor@gmail.com>
2020-02-01 17:41:10 +0100

    adapted for my needs

--- example/main.cpp
+++ example/main.cpp
@@ -19,32 +19,32 @@ int main(int argc, char *argv[]) {
 
     for(SubtitleItem * element : sub)
     {
-        myfile<<"BEGIN"<<endl;
-        myfile<<"startString : "<<element->getStartTimeString()<<endl;
-        myfile<<"start : "<<element->getStartTime()<<endl;
-        myfile<<"endString : "<<element->getEndTimeString()<<endl;
-        myfile<<"end : "<<element->getEndTime()<<endl;
-        myfile<<"text : "<<element->getText()<<endl;
-        myfile<<"justDialogue : "<<element->getDialogue()<<endl;
-        myfile<<"words count : "<<element->getWordCount()<<endl;
-        myfile<<"words :";
-        std::vector<std::string> word = element->getIndividualWords();
+//        myfile<<"BEGIN"<<endl;
+//        myfile<<"startString : "<<element->getStartTimeString()<<endl;
+//        myfile<<"start : "<<element->getStartTime()<<endl;
+//        myfile<<"endString: "<<element->getEndTimeString()<<endl;
+//        myfile<<"end : "<<element->getEndTime()<<endl;
+        myfile<<element->getText()<<endl;
+//        myfile<<"justDialogue : "<<element->getDialogue()<<endl;
+//        myfile<<"words count : "<<element->getWordCount()<<endl;
+//        myfile<<"words :";
+/*       std::vector<std::string> word = element->getIndividualWords();
             for(std::string display : word)
                 myfile<<display<<", ";
             myfile<<endl;
-
-        myfile<<"speakerCount : "<<element->getSpeakerCount()<<endl;
-        myfile<<"speakers : ";
-        if(element->getSpeakerCount())
+*/
+ //       myfile<<"speakerCount : "<<element->getSpeakerCount()<<endl;
+ //       myfile<<"speakers : ";
+/*        if(element->getSpeakerCount())
         {
             std::vector<std::string> name = element->getSpeakerNames();
             for(std::string display : name)
                 myfile<<display<<", ";
             myfile<<endl;
         }
-
-        myfile<<"ignore : "<<element->getIgnoreStatus()<<endl;
-        myfile<<"END"<<endl<<endl;
+*/
+   //     myfile<<"ignore : "<<element->getIgnoreStatus()<<endl;
+   //     myfile<<"END"<<endl<<endl;
     }
 
     return 0;
